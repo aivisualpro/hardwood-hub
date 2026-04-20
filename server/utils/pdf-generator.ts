5bdf58d50e333dd4a5cf1afe40ad1639bd245256
Adeel Jabbar <adeeljabbar@Adeels-MacBook-Pro-M5.local>
2026-04-20 20:25:09 +0500

    fixed contract download bugs2

--- server/utils/pdf-generator.ts
+++ server/utils/pdf-generator.ts
@@ -2,12 +2,26 @@ import puppeteer from 'puppeteer-core'
 import chromium from '@sparticuz/chromium'
 
 export const generatePdfFromHtml = async (htmlContent: string) => {
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(),
-    headless: chromium.headless as any,
-  })
+  const isLocal = process.env.NODE_ENV === 'development' || !process.env.VERCEL
+
+  let options: any = {}
+  if (isLocal) {
+    options = {
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+      headless: true,
+    }
+  } else {
+    chromium.setGraphicsMode = false
+    options = {
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless as any,
+    }
+  }
+
+  const browser = await puppeteer.launch(options)
 
   const page = await browser.newPage()
   await page.setContent(htmlContent, { waitUntil: 'networkidle0' })
