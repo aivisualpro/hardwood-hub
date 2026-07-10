af5d2a016bb66ce642dd1f8cce56b6bc6f3bb7de
Adeel Jabbar <admin@aivisualpro.com>
2026-07-10 15:39:02 +0500

    Clean up temporary migration and utility scripts

--- scripts/list-collections.mjs
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'
-import mongoose from 'mongoose'
-
-const uri = process.env.NUXT_MONGODB_URI
-await mongoose.connect(uri)
-const db = mongoose.connection.db
-const collections = await db.listCollections().toArray()
-console.log(collections.map(c => c.name))
-await mongoose.disconnect()
